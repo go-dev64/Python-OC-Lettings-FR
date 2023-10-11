d5e473e3ea27204e5f44f70459229df9b7f7419b
Oblin Guillaume <guillaume.oblin@sfr.fr>
2023-10-11 10:25:41 +0200

    move addres and lettng model from oc_lettings_site to lettings app

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Letting, Profile
+from .models import Profile
+from lettings.models import Letting
 
 
 # Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie quam lobortis leo consectetur ullamcorper non id est. Praesent dictum, nulla eget feugiat sagittis, sem mi convallis eros,
