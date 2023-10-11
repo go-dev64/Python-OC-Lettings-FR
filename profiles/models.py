b9df7a76be055e8987f00c25c14e8fd889868f7d
Oblin Guillaume <guillaume.oblin@sfr.fr>
2023-10-11 10:45:36 +0200

    move profile model from oc_lettings_site to prfiles app

--- profiles/models.py
+++ profiles/models.py
@@ -1,4 +1,4 @@
-"""from django.db import models
+from django.db import models
 from django.contrib.auth.models import User
 
 
@@ -8,4 +8,4 @@ class Profile(models.Model):
     favorite_city = models.CharField(max_length=64, blank=True)
 
     def __str__(self):
-        return self.user.username"""
+        return self.user.username
